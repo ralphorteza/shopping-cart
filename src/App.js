edcc398c80b69902ce85d05fba44c4d94e252318
ralph orteza <ralphlorteza@gmail.com>
2023-03-16 10:34:05 -0700

    Comment out Apps for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import './App.css';
 import Card from './components/Card';
 import data from "./data.js"
+
+// function App() {
+
+//   const items = data.data;
+//   const itemsArray = [];
+
+//   items.forEach( item => {
+//     itemsArray.push(
+//       {
+//         name: item.item.name,
+//         cost: item.item.cost,
+//         type: item.item.type,
+//         imageUrl: item.item.images.icon,
+//         id: item.itemId,
+//       }
+//     );
+//   });
+
+//   const cardsArray = itemsArray.map(item => (
+//     <Card key={item.id} itemObj={item}/>
+//   ));
+  
+//   return (
+//     <>
+//       <div className="container--card">{cardsArray}</div>
+//     </>
+//   );
+// }
+
 function App() {
 
   const items = data.data;
