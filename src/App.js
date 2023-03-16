6858b5d0e32679de4dd37bf43f4875b0ea1d3508
ralph orteza <ralphlorteza@gmail.com>
2023-03-16 13:26:05 -0700

    Modify Card elements and css

--- src/App.js
+++ src/App.js
@@ -2,62 +2,65 @@ import './App.css';
 import Card from './components/Card';
 import data from "./data.js"
 
-// function App() {
+function App() {
 
-//   const items = data.data;
-//   const itemsArray = [];
+  const items = data.data;
+  const itemsArray = [];
 
-//   items.forEach( item => {
-//     itemsArray.push(
-//       {
-//         name: item.item.name,
-//         cost: item.item.cost,
-//         type: item.item.type,
-//         imageUrl: item.item.images.icon,
-//         id: item.itemId,
-//       }
-//     );
-//   });
+  items.forEach( item => {
+    itemsArray.push(
+      {
+        name: item.item.name,
+        cost: item.item.cost,
+        type: item.item.type,
+        imageUrl: item.item.images.icon,
+        id: item.itemId,
+        description: item.item.description,
+        rating: item.item.ratings,
+      }
+    );
+  });
 
-//   const cardsArray = itemsArray.map(item => (
-//     <Card key={item.id} itemObj={item}/>
-//   ));
+  const cardsArray = itemsArray.map(item => (
+    <Card key={item.id} itemObj={item}/>
+  ));
   
-//   return (
-//     <>
-//       <div className="container--card">{cardsArray}</div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <div className="container--card">{cardsArray}</div>
+    </>
+  );
+}
 
 
 /* This App component is meant for testing purposes. Comment out the function
  * below and uncomment the function above if not in use.
  */
-function App() {
+// function App() {
 
-  const items = data.data;
-  const itemsArray = [];
-  const exampleCard =  {
-          name: items[8].item.name,
-          cost: items[8].item.cost,
-          type: items[8].item.type,
-          imageUrl: items[8].item.images.icon,
-          id: items[8].itemId,
-        };
+//   const items = data.data;
+//   const itemsArray = [];
+//   const exampleCard =  {
+//           name: items[8].item.name,
+//           cost: items[8].item.cost,
+//           type: items[8].item.type,
+//           imageUrl: items[8].item.images.icon,
+//           id: items[8].itemId,
+//           description: items[8].item.description,
+//           rating: items[8].item.ratings,
+//         };
   
-  itemsArray.push(exampleCard);
-  console.log(itemsArray);
-  
-  const cardsArray = itemsArray.map(item => (
-    <Card key={item.id} itemObj={item}/>
-  ));
+//   itemsArray.push(exampleCard);
 
-  return (
-    <>
-      {cardsArray}
-    </>
-  );
-}
+//   const cardsArray = itemsArray.map(item => (
+//     <Card key={item.id} itemObj={item}/>
+//   ));
+
+//   return (
+//     <>
+//       {cardsArray}
+//     </>
+//   );
+// }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Star from "../images/star.svg";
 
 export default function Card({ itemObj }) {
   console.log(itemObj)
@@ -9,9 +10,24 @@ export default function Card({ itemObj }) {
         alt="item to sell"
         src={itemObj.imageUrl}
       />
-      <p className="card--name">{itemObj.name}</p>
-      <p className="card--type">{itemObj.type}</p>
+      <div className="card--bottom">
+        <p className="card--name">{itemObj.name}</p>
+        <div className="card--rating">
+          <img
+          src={Star}
+          alt="rating star"
+          style={{ height: 20, width: 20}}
+          />
+          <span>{itemObj.rating.avgStars}</span>
+          <span style={{color: "gray"}}>({itemObj.rating.numberVotes})</span>
+          {/* <span className="card--type">{itemObj.type}</span> */}
+        </div>
+        <p className="card--type">{itemObj.type} <span></span></p>
+        <p className="card--description">{itemObj.description}</p>
+      </div>
       <p className="card--cost">₴{itemObj.cost}</p>
+      <div className="card--add">add</div>
     </div>
   );
 };
+
