2ab4762639a6703034288203eb145357250fde6e
ralph orteza <ralphlorteza@gmail.com>
2023-03-17 09:58:13 -0700

    Add Shop component and migrate curent App code to Shop

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import data from "./data.js"
 
 function App() {
 
-  const items = data.data;
-  const itemsArray = [];
+  // const items = data.data;
+  // const itemsArray = [];
 
-  items.forEach( item => {
-    itemsArray.push(
-      {
-        name: item.item.name,
-        cost: item.item.cost,
-        type: item.item.type,
-        imageUrl: item.item.images.icon,
-        id: item.itemId,
-        description: item.item.description,
-        rating: item.item.ratings,
-      }
-    );
-  });
+  // items.forEach( item => {
+  //   itemsArray.push(
+  //     {
+  //       name: item.item.name,
+  //       cost: item.item.cost,
+  //       type: item.item.type,
+  //       imageUrl: item.item.images.icon,
+  //       id: item.itemId,
+  //       description: item.item.description,
+  //       rating: item.item.ratings,
+  //     }
+  //   );
+  // });
 
-  const cardsArray = itemsArray.map(item => (
-    <Card key={item.id} itemObj={item}/>
-  ));
+  // const cardsArray = itemsArray.map(item => (
+  //   <Card key={item.id} itemObj={item}/>
+  // ));
   
-  return (
-    <>
-      <div className="container--card">{cardsArray}</div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="container--card">{cardsArray}</div>
+  //   </>
+  // );
 }
 
 
---
+++ src/components/Shop.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "./Card";
+import data from "../data";
+
+export default function Shop() {
+  const items = data.data;
+  const itemsArray = [];
+
+  items.forEach( item => {
+    itemsArray.push(
+      {
+        name: item.item.name,
+        cost: item.item.cost,
+        type: item.item.type,
+        imageUrl: item.item.images.icon,
+        id: item.itemId,
+        description: item.item.description,
+        rating: item.item.ratings,
+      }
+    );
+  });
+
+  const cardsArray = itemsArray.map(item => (
+    <Card key={item.id} itemObj={item}/>
+  ));
+  
+  return (
+    <>
+      <div className="container--card">{cardsArray}</div>
+    </>
+  );
+}
