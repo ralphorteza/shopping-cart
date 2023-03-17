d6da2cf0429ebd0d5a9cbe5c81df98f44b169570
ralph orteza <ralphlorteza@gmail.com>
2023-03-17 09:52:41 -0700

    Install react-router-dom and add RouteSwitch.js

---
+++ src/RouteSwitch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Shop from "./components/Shop"
+
+export default function RouteSwitch() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
