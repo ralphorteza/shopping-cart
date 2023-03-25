f7c65772679afcff8ea09d3abb44e1c5af08bb0b
ralph orteza <ralphlorteza@gmail.com>
2023-03-24 22:52:39 -0700

    Edit style

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Checkout.css"
 import CheckoutCard from "./CheckoutCard";
+import { Link } from "react-router-dom";
 
 export default function Checkout({
   cart,
@@ -22,6 +23,7 @@ export default function Checkout({
     <div className="checkout">
       <section className="checkout--info grid">
         <h3 className="checkout--subtotal">SubTotal: ₴34,444</h3>
+        <Link className="link--shop button" to="/shop">Continue Shopping</Link>
         <button className="button">Checkout</button>
       </section>
       <section className="checkout--cards grid">
