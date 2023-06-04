3200f878b65bb92707be8aa99532d14d227af3e6
ralph orteza <ralphlorteza@gmail.com>
2023-06-03 17:27:09 -0700

    Edit NavBar mobile styling

--- src/components/page-elements/Nav.js
+++ src/components/page-elements/Nav.js
@@ -12,15 +12,6 @@ export default function Nav() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  let amountItem;
-  if (cartReview.quantity >= 1 && cartReview.quantity < 100) {
-    amountItem = (<span className="dot">{cartReview.quantity}</span>);
-  } else if ( cartReview.quantity >= 100) {
-    amountItem = (<span className="dot">99+</span>)
-  } else{
-    amountItem = ("");
-  }
-
   async function handleLogout() {
     setError('');
 
@@ -36,15 +27,56 @@ export default function Nav() {
     navigate('/login');
   }
 
+  const loginButton = (
+    location.pathname !== '/signup' &&
+    location.pathname !== '/login' &&
+    !currentUser && 
+    <button
+      className="login button"
+      onClick={handleLogin}
+    >
+      Login
+    </button>
+  );
+
+  const logoutButton = (
+    <button
+      className="logout button"
+      onClick={handleLogout}
+    >
+      logout
+    </button>
+  );
+
+  const profileLink = (
+    currentUser &&
+    location.pathname !== '/update-profile' && 
+    <Link
+      className="account--link"
+      to="/update-profile"
+    >
+      {currentUser.email}
+    </Link>
+  );
+
+  let amountItem;
+  if (cartReview.quantity >= 1 && cartReview.quantity < 100) {
+    amountItem = (<span className="dot">{cartReview.quantity}</span>);
+  } else if ( cartReview.quantity >= 100) {
+    amountItem = (<span className="dot">99+</span>)
+  } else{
+    amountItem = ("");
+  }
+
   return (
     <nav className="navbar grid">
       <Link className="logo" to="/">
         <h1>FortBuy</h1>
       </Link>
       {error && <h1>{error}</h1>}
-      {location.pathname !== '/signup' && location.pathname !== '/login' && !currentUser && <button onClick={handleLogin}>Login</button>}
-      {currentUser && location.pathname !== '/update-profile' && <Link to="/update-profile">{currentUser.email}</Link>} 
-      {currentUser && <button onClick={handleLogout}>logout</button>}
+      {profileLink}
+      {loginButton}
+      {currentUser && logoutButton}
       <Link className="cart--link" to="/checkout">
         <img className="cart" src={Cart} alt="checkout cart"></img>{amountItem}
       </Link>
